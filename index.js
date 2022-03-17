b1b780369a487417199fb036f24f4b27a0f56061
Xavier Georget <xageorget@gmail.com>
2022-03-17 12:06:10 +0100

    solution for chalk workshop

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Vincent"));
+console.log(chalk.red("Brice"));
+console.log(chalk.green("Séverine"));
+console.log(chalk.bgMagenta.blue("Erwan"));
